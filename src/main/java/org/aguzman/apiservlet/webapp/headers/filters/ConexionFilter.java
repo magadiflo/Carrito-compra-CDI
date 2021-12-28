3921e4a0bf617dca67892570b6883cea9718cfae
Martín <magadiflo@gmail.com>
2021-12-28 18:04:27 -0500

    Manejo de transacciones usando interceptores y anotaciones

--- src/main/java/org/aguzman/apiservlet/webapp/headers/filters/ConexionFilter.java
+++ src/main/java/org/aguzman/apiservlet/webapp/headers/filters/ConexionFilter.java
@@ -1,41 +1,22 @@
 package org.aguzman.apiservlet.webapp.headers.filters;
 
-import jakarta.inject.Inject;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletResponse;
-import org.aguzman.apiservlet.webapp.headers.configs.MysqlConn;
 import org.aguzman.apiservlet.webapp.headers.services.ServiceJdbcException;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.SQLException;
 
 @WebFilter("/*")
 public class ConexionFilter implements Filter {
 
-    @Inject
-    @MysqlConn
-    private Connection conn;
-
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        try{
-            Connection connRequest = this.conn;
-            if(connRequest.getAutoCommit()){
-                connRequest.setAutoCommit(false);
-            }
-            try{
-                chain.doFilter(request, response);
-                connRequest.commit();
-            } catch (SQLException | ServiceJdbcException e){
-                connRequest.rollback();
-                ((HttpServletResponse)response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
-                e.printStackTrace();
-            }
-        } catch (SQLException e) {
+        try {
+            chain.doFilter(request, response);
+        } catch (ServiceJdbcException e) {
+            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
             e.printStackTrace();
         }
     }
-
 }
